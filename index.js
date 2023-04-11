ce1e40e091dc005f2b257a3b7d4f52db1859d065
Marco Iannuzzi <marcoiannuzzi@live.com.ar>
2023-04-10 22:28:24 -0300

    api

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ $('.dark').click( function(){
     $('.dark').addClass('invisible');
     $('.light').removeClass('invisible');
     $('.light').addClass('text-light');
-})
+});
 
 $('.light').click( function(){
     $('html').attr('data-bs-theme','light');
     $('.light').addClass('invisible');
     $('.dark').removeClass('invisible');
-})
+});
+
+const API_URL = 'https://643478b3582420e2317ceb41.mockapi.io';
+const APIRESPONSE = document.querySelector('#apiresponse');
+
+fetch(`${API_URL}/Teams`)
+  .then(team => team.json())
+  .then(equipo => {
+        const todoElEquipo = equipo.map(equipo => `<div class="card" style="width: 18rem;">
+        <img src="./img/TEAMIT-serviciosinfraestructura.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${equipo.nombre} ${equipo.apellido}</h5>
+          <p class="card-text">${equipo.data}</p>
+          <a href="${equipo.linkedin}" class="btn btn-primary" target="_blank">LinkedIn</a>
+        </div>
+      </div>`);
+        APIRESPONSE.innerHTML = todoElEquipo;
+        console.log(APIRESPONSE);
+    }); 
 
